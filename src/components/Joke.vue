718033ae0dd4142b187bd5c5d03d681cbe5d5d5e
Rust Talapov <rust.talapov@gmail.com>
2018-04-25 12:01:41 +0300

    initial commit

---
+++ src/components/Joke.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-md-6">
+        <div class="joke-card">
+            <button class="close" @click="removeJoke(index)"><span>&times;</span></button>
+            <div class="card-block">
+                <p><u>{{ joke.setup }}</u></p>
+                <p><em>{{ joke.punchline }}</em></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: ['joke', 'index'],
+    methods: mapActions([
+      'removeJoke'
+    ])
+  }
+</script>
